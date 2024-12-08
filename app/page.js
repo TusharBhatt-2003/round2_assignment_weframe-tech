75e243d3af67e076c57dec96f756b1a3c8a1ff30
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-12-09 03:04:03 +0530

    Update

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex gap-2 pl-5 pt-2 items-baseline">
+      <div className="sticky top-32 z-50 bg-white flex gap-2 pl-5 py-2 items-baseline">
         <h1 className="text-[#393939]">Home</h1>
         <div className="w-2 h-2 rounded-full bg-[#e2e2e2]"></div>
         <p className="text-[#9c9c9c] font-semibold text-sm">Art de la table</p>

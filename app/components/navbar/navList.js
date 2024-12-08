7c42d6eef7e1ebcd9ae52cf31dc909b31d0de4a3
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-12-08 06:39:11 +0530

    Update

--- app/components/navbar/navList.js
+++ app/components/navbar/navList.js
@@ -12,22 +12,43 @@ const NavList = () => {
   };
 
   return (
-    <nav className="lg:flex justify-between pt-5 font-['poppins'] w-full hidden">
-      {navList.map((item, index) => (
-        <a
-          key={index}
-          href={`#${item.replace(/\s+/g, "-").toLowerCase()}`} // Dynamically setting href based on item name
-          className={`text-xs ${
-            activeIndex === index
-              ? "text-[#0093d0] font-semibold"
-              : "text-gray-500"
-          } hover:text-gray-700`}
-          onClick={() => handleClick(index)}
-        >
-          {item}
-        </a>
-      ))}
-    </nav>
+    <>
+      {/* Desktop navigation */}
+      <nav className="hidden lg:flex justify-between pt-5 font-['poppins'] w-full">
+        {navList.map((item, index) => (
+          <a
+            key={index}
+            href={`#${item.replace(/\s+/g, "-").toLowerCase()}`} // Dynamically setting href based on item name
+            className={`text-xs ${
+              activeIndex === index
+                ? "text-[#0093d0] font-semibold"
+                : "text-gray-500"
+            } hover:text-gray-700`}
+            onClick={() => handleClick(index)}
+          >
+            {item}
+          </a>
+        ))}
+      </nav>
+
+      {/* Mobile navigation as a horizontal slider */}
+      <nav className="flex lg:hidden overflow-x-scroll whitespace-nowrap space-x-4 pt-5 font-['poppins'] w-full scrollbar-hide">
+        {navList.map((item, index) => (
+          <a
+            key={index}
+            href={`#${item.replace(/\s+/g, "-").toLowerCase()}`} // Dynamically setting href based on item name
+            className={`text-xs ${
+              activeIndex === index
+                ? "text-[#0093d0] font-semibold"
+                : "text-gray-500"
+            } hover:text-gray-700`}
+            onClick={() => handleClick(index)}
+          >
+            {item}
+          </a>
+        ))}
+      </nav>
+    </>
   );
 };
 
